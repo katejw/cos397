8ca6f48a22be5c213e9c567690408f5757e84cff
katejw <154714402+katejw@users.noreply.github.com>
2025-01-09 13:53:13 -0500

    Update frequencytxt.py

--- frequencytxt.py
+++ frequencytxt.py
@@ -1,18 +1,18 @@
 from collections import Counter
 from konlpy.tag import Okt
 
-def freq(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+def freq(file):
+    # read text
+    with open(file, 'r', encoding='utf-8') as file:
         text = file.read().lower()
 
+    # tokenize, extract nouns, then count word frequency
     okt = Okt()
-    # extract nouns
     tokens = okt.nouns(text)
-    # count word frequency
     word_counts = Counter(tokens)
 
     for word, count in word_counts.most_common(15):
         print(f'{word}: {count}')
 
-file_path = 'character_drugs_m.txt'
-freq(file_path)
+file = 'character_drugs_m.txt'
+freq(file)
